b419c1d36a353ae4b8513453f9c091c9f2034675
Tara Ram Mohan <19trammohan@gmail.com>
2020-09-15 06:58:57 -0400

    Added TrophyCase screen and Edit/Add Events Pop-Up

---
+++ app/src/main/java/com/example/kidstodoapp/ui/main/EditEventsFragment.java
@@ -0,0 +1,35 @@
+package com.example.kidstodoapp.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.kidstodoapp.R;
+
+public class EditEventsFragment extends Fragment {
+
+    public static EditEventsFragment newInstance() {
+        return new EditEventsFragment();
+    }
+
+    private MainViewModel mViewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
