bed0dea6a93530046cb622820574e061e43830f4
Shaquan Hagans <54368754+haganssd@users.noreply.github.com>
2020-09-17 19:42:16 -0400

    Testing the List

--- app/src/main/java/com/example/kidstodoapp/MainActivity.java
+++ app/src/main/java/com/example/kidstodoapp/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.kidstodoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 // This comment was made by Sean Youngstone
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.toDoList);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-}
-
-public static void main(String[] args) {
-
-    String cookies = 1234;
-    
 }
