41ec9db96fc7b1865fd25fe8ddcd37e87c09a5fb
Tara Ram Mohan <19trammohan@gmail.com>
2020-09-17 22:15:16 -0400

    Edit Pop-up Code Changes

--- app/src/main/java/com/example/kidstodoapp/ui/main/EditEventsFragment.java
+++ app/src/main/java/com/example/kidstodoapp/ui/main/EditEventsFragment.java
@@ -2,38 +2,61 @@ package com.example.kidstodoapp.ui.main;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.example.kidstodoapp.R;
 
-public class EditEventsFragment extends Fragment {
+public class EditEventsFragment extends Activity {
 
-    private MainViewModel mViewModel;
-
-    public static EditEventsFragment newInstance() {
-        return new EditEventsFragment();
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
-    }
+    PopupWindow popUp;
+    boolean click = true;
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        popUp = new PopupWindow(this);
+        final LinearLayout layout = new LinearLayout(this);
+        LinearLayout mainLayout = new LinearLayout(this);
+        TextView tv = new TextView(this);
+        Button button = new Button(this);
+        button.setText("Edit");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (click) {
+                    popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+                    popUp.update(50, 50, 300, 80);
+                    click = false;
+                } else {
+                    popUp.dismiss();
+                    click = true;
+                }
+            }
+        });
 
-}
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        tv.setText("Hi this is a sample text for popup window");
+        layout.addView(tv, params);
+        popUp.setContentView(layout);
+        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+        mainLayout.addView(button, params);
+        setContentView(mainLayout);
+            }
+        }
+
