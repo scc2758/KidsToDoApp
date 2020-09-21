b7ab25a040f8fe202b07f0d356bdecf83658e09d
scc2758 <70292138+scc2758@users.noreply.github.com>
2020-09-20 20:16:57 -0400

    Adding GridLayout

--- app/src/main/java/com/example/kidstodoapp/MainActivity.java
+++ app/src/main/java/com/example/kidstodoapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.kidstodoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 // This comment was made by Sean Youngstone
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button trophyBtn = (Button) findViewById(R.id.trophyBtn);
+
+        trophyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), TrophyCase.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kidstodoapp/TrophyCase.java
+++ app/src/main/java/com/example/kidstodoapp/TrophyCase.java
@@ -1,7 +1,12 @@
 package com.example.kidstodoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,6 +20,14 @@ public class TrophyCase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy_case);
+        Button homeBtn = (Button) findViewById(R.id.homeBtn);
+        TextView earnedText = (TextView) findViewById(R.id.earnedText);
+        TextView availableText = (TextView) findViewById(R.id.availableText);
+        GridLayout earnedGrid = (GridLayout) findViewById(R.id.earnedGrid);
+        GridLayout availableGrid = (GridLayout) findViewById(R.id.availableGrid);
+        ImageButton earnedPrize = (ImageButton) findViewById(R.id.earnedPrize);
+        ImageButton availablePrize = (ImageButton) findViewById(R.id.availablePrize);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +37,13 @@ public class TrophyCase extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startIntent);
             }
         });
     }
