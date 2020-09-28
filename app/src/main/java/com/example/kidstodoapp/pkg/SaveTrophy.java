66686b51800a63ee48d7634bc28f6c105c76028a
Tara Ram Mohan <19trammohan@gmail.com>
2020-09-28 11:19:19 -0400

    Create distinct Create and Save Trophy pages

--- app/src/main/java/com/example/kidstodoapp/pkg/SaveTrophy.java
+++ app/src/main/java/com/example/kidstodoapp/pkg/SaveTrophy.java
@@ -1,5 +1,7 @@
 package com.example.kidstodoapp.pkg;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.kidstodoapp.R;
@@ -9,9 +11,12 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,7 +33,20 @@ public class TrophyPopUp extends AppCompatActivity {
     String pointString = pointsInput.getText().toString();
     int points = Integer.parseInt(pointString);
 
+    //Image Gallery
+    private static final int PICK_IMAGE = 100;
+    ImageView imageViewGallery;
+    Button buttonGallery;
+    Uri imageUriGallery;
 
+    /** Called when the activity is first created. */
+    TextView showPopUpButton; //NEW
+    EditText getInput; //NEW
+    EditText getInput2; //NEW
+    // declare string to save the dialog edittext
+    String myValue = "" ;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //Delete toolbar
@@ -49,9 +67,10 @@ public class TrophyPopUp extends AppCompatActivity {
                 Trophy ttemp = new Trophy(nameString,points);
                 trophyCase.put(idnumber, ttemp);
                 idnumber++;
+
             }
         });
-
+        
         //Save edits made to trophy icon
         Button saveButton = findViewById(R.id.saveButton);
         newButton.setOnClickListener(new View.OnClickListener() {
@@ -67,7 +86,29 @@ public class TrophyPopUp extends AppCompatActivity {
             }
         });
 
+        imageViewGallery = (ImageView)findViewById(R.id.imageView);
+        buttonGallery = (Button)findViewById(R.id.changeIcon);
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
         }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUriGallery = data.getData();
+            imageViewGallery.setImageURI(imageUriGallery);
+        }
+    }
 
 
 
