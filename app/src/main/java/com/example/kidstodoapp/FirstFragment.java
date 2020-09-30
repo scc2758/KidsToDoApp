04bb66ce115c56f6e5a7108f79c600d979c12764
syoungstone <syoungstone@gmail.com>
2020-09-30 13:59:36 -0400

    Revert "io am absolutely destroying this file"
    This reverts commit ca5e4508f1005619c3df20cc5951a8f28afec22f.

--- app/src/main/java/com/example/kidstodoapp/FirstFragment.java
+++ app/src/main/java/com/example/kidstodoapp/FirstFragment.java
@@ -11,22 +11,21 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
-    @Overrideio am absolutely destroying this file
+    @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup conio am absolutely destroying this filetainer,
+            LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
-    ) {io am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this file
+    ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
-io am absolutely destroying this file
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        io am absolutely destroying this file
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListeneio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this filer() {
-            @Overrideio am absolutely destroying this file
-            public void onClick(View view) {io am absolutely destroying this file
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
