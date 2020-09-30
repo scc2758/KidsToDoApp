ca5e4508f1005619c3df20cc5951a8f28afec22f
El Makes Games 3 <43424070+PastelGames@users.noreply.github.com>
2020-09-30 13:49:11 -0400

    io am absolutely destroying this file
    io am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this file

--- app/src/main/java/com/example/kidstodoapp/FirstFragment.java
+++ app/src/main/java/com/example/kidstodoapp/FirstFragment.java
@@ -11,21 +11,22 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
-    @Override
+    @Overrideio am absolutely destroying this file
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            LayoutInflater inflater, ViewGroup conio am absolutely destroying this filetainer,
             Bundle savedInstanceState
-    ) {
+    ) {io am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this file
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
-
+io am absolutely destroying this file
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        io am absolutely destroying this file
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListeneio am absolutely destroying this fileio am absolutely destroying this fileio am absolutely destroying this filer() {
+            @Overrideio am absolutely destroying this file
+            public void onClick(View view) {io am absolutely destroying this file
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
