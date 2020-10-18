6125042de5dc375ba7c56a445328a5ba9fec0118
rammohant <19trammohan@gmail.com>
2020-10-17 22:00:03 -0400

    In-class

---
+++ app/src/main/java/com/example/kidstodoapp/TextChanging.java
@@ -0,0 +1,14 @@
+package com.example.kidstodoapp;
+
+public class TextChanging {
+
+    String temp;
+
+    public
+
+    for (int i = 0; i < temp.length; i++) {
+        temp.charAt(i)
+    }
+
+
+}
